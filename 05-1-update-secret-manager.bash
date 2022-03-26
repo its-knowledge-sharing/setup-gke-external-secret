52cf3d7e90ea68c2f904acb1720621c3417693b8
seubpong.mon <pjame.fb@gmail.com>
2022-03-26 14:54:41 +0700

    Update Secret Manager

--- 05-1-update-secret-manager.bash
+++ 05-1-update-secret-manager.bash
@@ -6,7 +6,7 @@ SECRET_NAME=gcp-demo-secret-key
 TEST_CREDENTIAL1=$(openssl rand -hex 8)
 TEST_CREDENTIAL2=$(openssl rand -hex 8)
 
-gcloud secrets --project ${PROJECT} update ${SECRET_NAME} --replication-policy="automatic"
+gcloud secrets --project ${PROJECT} update ${SECRET_NAME}
 
 cat << EOF | gcloud secrets --project ${PROJECT} versions add ${SECRET_NAME} --data-file=-
 {
